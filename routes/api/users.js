3589766509effe924787138d641a7472d1dc5a77
Keeth <keeth.spindler@gmail.com>
2020-09-12 21:29:04 -0400

    Simple Route Construction

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,3 +7,5 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('User Route');
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,5 +7,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API RUNNING'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
