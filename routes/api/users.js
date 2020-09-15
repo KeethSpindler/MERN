ebcced4e17ea65e1dedafb059c01898c5a60bd76
Keeth <keeth.spindler@gmail.com>
2020-09-14 20:04:55 -0400

    Create/Update Profile Routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 const config = require('config');
 
 const User = require('../../modles/User');
+
 //@route     POST api/users
 //@desc      Register user
 //@access    Public
